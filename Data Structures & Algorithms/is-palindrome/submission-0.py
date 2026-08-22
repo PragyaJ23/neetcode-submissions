d8ed5f6d51abc989cf2e5d886c6a5e721b672503
PragyaJ23 <pragyajmera23@gmail.com>
2026-08-22 11:25:12 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        t = ''.join(char.lower()for char in s if char.isalnum())
+        left = 0
+        right = len(t) - 1
+        while left < right:
+            if t[left] != t[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+p = Solution()
+s = "Was it a car or a cat I saw?"
+print(p.isPalindrome(s))
+
+        
